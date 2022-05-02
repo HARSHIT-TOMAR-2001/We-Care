5dddcbbb2e54e34eadf2cd723f727e628d09a187
HarshitTomar2001 <harshittomar1@gmail.com>
2022-05-02 21:40:57 +0530

    backend and frontend structure  for the project

---
+++ we_care_backend/src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Router = require("./routers");
+const cors = require("cors");
+require("dotenv").config();
+// const connectDB = require("./database/connect")();
+// const User = require("./models/user.model");
+// const Chat = require("./models/chat.model");
+ 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.use("/api", Router);
+
+let port = process.env.PORT || 4000;
+
+const server = app.listen(port, function () {
+  console.log("Server is up and running at port:", port);
+});
---
+++ we_care_frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      We Care!!!
+    </div>
+  );
+}
+
+export default App;
