deb01decafba81374d64184712a6684255d4fdf9
saarthak <saarthaksingh09@gmail.com>
2022-05-15 14:49:30 +0530

    donor model added

---
+++ we_care_backend/src/models/blood.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const donorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  contact: { type: Number, required: true,  },
+  age: { type: Number, required: true  },
+  gender: { type: String, required:true },
+  bloodGroup:{type: String, required:true},
+  city: { type: String, required:true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const Donor = mongoose.model("Donor", donorSchema);
+module.exports = Doctor;
