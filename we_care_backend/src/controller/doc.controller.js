59260c50e0e1ae330f176cddcb1e14f450861548
saarthak <saarthaksingh09@gmail.com>
2022-05-17 08:18:23 +0530

    doc controller modified

--- we_care_backend/src/controller/doc.controller.js
+++ we_care_backend/src/controller/doc.controller.js
@@ -3,7 +3,6 @@ const Doctor = require("../models/doc.model");
 const register=async (req,res)=>{
   console.log(req.body);
   const  {name,contact_details,degree,specialization,city,address,email_id,password}=req.body;
-  console.log("mzaa agya bro");
   try {
     const doctorExist = await Doctor.exists({ email: email_id });
     if (doctorExist) {
@@ -51,40 +50,30 @@ async function loginemail(req, res) {
   
   
 const create_slot=async (req,res)=>{
-  console.log(req.body);
-  const  {name,contact_details,degree,specialization,city,address,email_id,password}=req.body;
-  console.log("mzaa agya bro");
+  // console.log(req.body);
+  // const  {name,contact_details,degree,specialization,city,address,email_id,slots}=req.body;
   try {
-    const doctorExist = await Doctor.exists({ email: email_id });
-    if (doctorExist) {
-      return res.status(400).send({ msg: "user already registered",status:0 });
+    const updatedSlot= await Doctor.updateOne({email:req.body.email_id},{$push:{slots:{start_time:req.body.start_time, end_time:req.body.end_time}}},{$inc:{slots:{curr_limit:1}}})
+    if (!updatedSlot) {
+      return res.status(400).send({ msg: "cannot update slot",status:0 });
     }
     //process.env.Salt
     // const hashedPassword = await bcrypt.hash(record.password, 10);
-    var newDoctor = await Doctor.create({
-      name: name,
-      contact:parseInt(contact_details),
-      degree:degree,
-      specialization:specialization,
-      city:city,
-      address:address,
-      email:email_id,
-      password:password,
-    });
 
+    
     // const token = await generateToken(newUser);
 
     // newUser.token = token;
-    console.log("Doctor created!");
+    // console.log("Doctor created!");
     // res.json({
     //   message: "Doctor [" + name + "] registered successfully!",
     // });
-    res.status(200).send(newDoctor);
+    res.status(200).send(updatedSlot);
   } catch (error) {
     res.status(400).send(error.message);
   }
 }
 
-module.exports={register,loginemail}
+module.exports={register,loginemail, create_slot}
  
 
--- we_care_backend/src/routers/doc.router.js
+++ we_care_backend/src/routers/doc.router.js
@@ -1,12 +1,14 @@
 const {
     register,
- loginemail
+ loginemail,
+ create_slots
   } = require("../controller/doc.controller");
   
   const docauthRouter = require("express").Router();
   
   docauthRouter.post("/signup", register);
-  docauthRouter.post("/loginemail", loginemail);
+  docauthRouter.post("/loginemail", loginemail)
+  docauthRouter.post('/slots/:id',create_slots)
   // authRouter.post("/sendotp", sendotp);
   // authRouter.post("/verifyotp", verifyotp);
   module.exports = docauthRouter;
