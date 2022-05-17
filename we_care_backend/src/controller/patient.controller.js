c08935f3310b7d6a25d82d85673e991021edc259
saarthak <saarthaksingh09@gmail.com>
2022-05-17 11:21:46 +0530

    patient controller modified

--- we_care_backend/src/controller/patient.controller.js
+++ we_care_backend/src/controller/patient.controller.js
@@ -1,3 +1,4 @@
+const Doctor = require("../models/doc.model");
 const Patient = require("../models/patient.model");
 
 const register=async (req,res)=>{
@@ -42,8 +43,47 @@ async function loginemail(req, res) {
   
 
   }
+
+
+  const book_slot=async (req,res)=>{
+    // console.log(req.body);
+    const slot_id=req.body.slot_id
+    const doc_email=req.body.doc.email
+    const patient_email=req.body.patient.email
+
+    const  {name,contact_details,degree,specialization,city,address,email_id,slots}=req.body;
+    try {
+      const doctor=await Doctor.findOne({email:doc_email})
+
+      if(doctor.slot_id.curr_limit<doctor.slot_id.max_limit){
+        const updatedSlot= await Doctor.updateOne({email:doc_email, slots:slot_id},{$inc:{curr_limit:1}})
+        const updatedPatient=await Patient.updateOne({email:patient_email}, {$push:{slots:{start_time:req.body.start_time, end_time:req.body.end_time}}})
+      }
+      else{
+        return res.send({msg:"All slots are booked."})
+      }
+      // const updatedSlot= await Doctor.updateOne({email:req.body.email_id},{$push:{slots:{start_time:req.body.start_time, end_time:req.body.end_time,max_limit:req.body.max_limit}}})
+      if (!updatedSlot || updatedPatient) {
+        return res.status(400).send({ msg: "cannot update slot",status:0 });
+      }
+      //process.env.Salt
+      // const hashedPassword = await bcrypt.hash(record.password, 10);
+  
+      
+      // const token = await generateToken(newUser);
+  
+      // newUser.token = token;
+      // console.log("Doctor created!");
+      // res.json({
+      //   message: "Doctor [" + name + "] registered successfully!",
+      // });
+      res.status(200).send(updatedSlot);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  }
   
 
-module.exports={register,loginemail}
+module.exports={register,loginemail,book_slot}
  
 
--- we_care_backend/src/routers/patient.router.js
+++ we_care_backend/src/routers/patient.router.js
@@ -1,12 +1,14 @@
 const {
     register,
- loginemail
+ loginemail,
+ book_slot
   } = require("../controller/patient.controller");
   
   const patientauthRouter = require("express").Router();
   
   patientauthRouter.post("/signup", register);
   patientauthRouter.post("/loginemail", loginemail);
+  patientauthRouter.post('/bookslot',book_slot)
   // authRouter.post("/sendotp", sendotp);
   // authRouter.post("/verifyotp", verifyotp);
   module.exports = patientauthRouter;
